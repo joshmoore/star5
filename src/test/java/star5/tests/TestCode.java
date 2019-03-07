a5d2a5a1d318e654034b9ac16ade3cf54d5424a3
jmoore <josh@glencoesoftware.com>
2019-03-07 14:28:32 +0100

    Minor improvements to the test

--- src/test/java/star5/tests/TestCode.java
+++ src/test/java/star5/tests/TestCode.java
@@ -10,17 +10,29 @@ import star5.Partition;
 import star5.StorageDescriptor;
 import star5.callbacks.PartitionCallback;
 
+import java.io.File;
+
 public class TestCode {
 
     @Test
     public void testFromRAI() throws Exception {
-        StorageDescriptor sd = new MultiFileHDF5StorageDescriptor("", "/tmp/test-x[%d-%d]-y[%d-%d]-z[%d-%d].h5",
-                "data", new long[]{512, 512, 5}, new int[]{256, 256, 5}, null);
-        ArrayImg<ByteType, ByteArray> rai = ArrayImgs.bytes(1024, 1024, 10);
+        StorageDescriptor sd = new MultiFileHDF5StorageDescriptor(
+                "",
+                "/tmp/test-xs%04d-xe%04d-ys%04d-ye%04d-zs%04d-ze%04d-cs%d-ce%d-ts%02d-te%02d.h5",
+                "data",
+                new long[]{1024, 1024, 5, 1, 1},
+                new int[]{256, 256, 5, 1, 1},
+                null);
+        ArrayImg<ByteType, ByteArray> rai = ArrayImgs.bytes(1024, 1024, 10, 3, 6);
         sd.saveRAI(rai, new PartitionCallback(){
             @Override
             public void afterPartition(Partition partition) {
-                System.out.println(partition.getPath());
+                File tmp = new File(partition.getPath());
+                if (!tmp.exists()) {
+                    throw new RuntimeException("didn't find file:" + tmp);
+                } else {
+                    tmp.deleteOnExit();
+                }
             }
         });
     }
